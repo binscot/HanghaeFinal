785559ec64e6c3faf25f14f647cc2815dcd763ef
taekgyu <taekgyu0602@gmail.com>
2022-03-02 21:15:06 +0900

    pull전 commit

---
+++ src/main/java/com/example/hanghaefinal/repository/PostLikesRepository.java
@@ -0,0 +1,7 @@
+package com.example.hanghaefinal.repository;
+
+import com.example.hanghaefinal.model.PostLikes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostLikesRepository extends JpaRepository<PostLikes, Long> {
+}
