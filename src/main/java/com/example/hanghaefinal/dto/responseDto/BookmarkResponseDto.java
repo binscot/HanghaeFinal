14998c75417fc180b2fa488bf25328021c662904
fnzl08 <nero923@gmail.com>
2022-03-17 15:00:51 +0900

    hj 북마크 반환값 수정

--- src/main/java/com/example/hanghaefinal/dto/responseDto/BookmarkResponseDto.java
+++ src/main/java/com/example/hanghaefinal/dto/responseDto/BookmarkResponseDto.java
@@ -1,20 +1,19 @@
 package com.example.hanghaefinal.dto.responseDto;
 
+import com.example.hanghaefinal.model.Post;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class BookmarkInfoResponseDto {
+public class BookmarkResponseDto {
     private final Long id;
-    private final Long postId;
+    private final Post post;
     private final Long userId;
 
-    public BookmarkInfoResponseDto(Long id, Long postId, Long userId){
+    public BookmarkResponseDto(Long id, Post post, Long userId){
         this.id = id;
-        this.postId = postId;
+        this.post = post;
         this.userId = userId;
     }
 }
-
-
