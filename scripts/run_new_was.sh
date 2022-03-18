6c8e5a9c18d38c17a151c9c2bc01f6a4c53e48f1
taekgyu <taekgyu0602@gmail.com>
2022-03-18 21:13:04 +0900

    nginx 설정 파일 수정

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -24,6 +24,6 @@ if [ ! -z ${TARGET_PID} ]; then
 fi
 
 #마지막&는 프로세스가 백그라운드로 실행되도록 해준다.
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/playground-logging/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/playground-logging/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,55 +1,31 @@
 #!/bin/bash
-echo "> 현재 구동중인 Port 확인"
-CURRENT_PROFILE=$(curl -s http://localhost/profile)
 
-if [ $CURRENT_PROFILE == set1 ]
-then
-  IDLE_PORT=8082
-elif [ $CURRENT_PROFILE == set2 ]
-then
-  IDLE_PORT=8081
+# Crawl current connected port of WAS
+
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+echo "> Nginx currently proxies to ${CURRENT_PORT}."
+
+# Toggle port number
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+  TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+  TARGET_PORT=8081
 else
-  echo "> 일치하는 Profile이 없습니다. Profile:$CURRENT_PROFILE"
-  echo "> 8081을 할당합니다."
-  IDLE_PORT=8081
+  echo "> No WAS is connected to nginx"
+  exit 1
 fi
 
-PROXY_PORT=$(curl -s http://localhost/profile)
-echo "> 현재 구동중인 Port: $PROXY_PORT"
+# Change proxying port into target port
+# tee 는
+  # 출력 내용을 파일로 만들어주는 커맨드입니다.
+  # 새로 띄운 WAS의 포트를 nginx가 읽을 수 있도록 service_url.inc에 내용을 덮어씁니다.
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ubuntu/service_url.inc
 
-echo "> 전환할 Port : $IDLE_PORT"
-echo "> Port 전환"
-echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+echo "> Now Nginx proxies to ${TARGET_PORT}."
 
-echo "> Nginx Reload"
+# Reload nginx (nginx 서버의 재시작 없이 바로 새로운 설정값으로 서비스를 이어나갈 수 있도록 합니다.)
 sudo service nginx reload
 
-
-##!/bin/bash
-#
-## Crawl current connected port of WAS
-#CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
-#TARGET_PORT=0
-#
-#echo "> Nginx currently proxies to ${CURRENT_PORT}."
-#
-## Toggle port number
-#if [ ${CURRENT_PORT} -eq 8081 ]; then
-#  TARGET_PORT=8082
-#elif [ ${CURRENT_PORT} -eq 8082 ]; then
-#  TARGET_PORT=8081
-#else
-#  echo "> No WAS is connected to nginx"
-#  exit 1
-#fi
-#
-## Change proxying port into target port
-#echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ec2-user/service_url.inc
-#
-#echo "> Now Nginx proxies to ${TARGET_PORT}."
-#
-## Reload nginx
-#sudo service nginx reload
-#
-#echo "> Nginx reloaded."
-#
+echo "> Nginx reloaded."
