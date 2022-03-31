1b79f6e4b95cdc4e404ae39e14032594bf36dd10
fnzl08 <nero923@gmail.com>
2022-03-31 19:12:35 +0900

    hj global ex

--- src/main/java/com/example/hanghaefinal/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/hanghaefinal/exception/GlobalExceptionHandler.java
@@ -46,11 +46,13 @@ public class GlobalExceptionHandler {
                 restApiException,
                 HttpStatus.BAD_REQUEST
         );
+
     }
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException e) {
         return new ResponseEntity<>(new ErrorResponse("U001", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler(UsernameNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUsernameNotFoundException(UsernameNotFoundException e) {
         return new ResponseEntity<>(new ErrorResponse("U001", e.getMessage()), HttpStatus.BAD_REQUEST);
@@ -64,72 +66,72 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CommentLimitException.class)
     public ResponseEntity<ErrorResponse> handleCommentLimitException(CommentLimitException e){
-        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.BAD_REQUEST);
 
     }
     @ExceptionHandler(CommentNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleCommentNotFoundException(CommentNotFoundException e){
-        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.BAD_REQUEST);
 
     }
     @ExceptionHandler(ContentNullException.class)
     public ResponseEntity<ErrorResponse> handleContentNullException(ContentNullException e){
-        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.BAD_REQUEST);
 
     }
     @ExceptionHandler(EqualPasswordException.class)
     public ResponseEntity<ErrorResponse> handleEqualPasswordException(EqualPasswordException e){
-        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.BAD_REQUEST);
 
     }
     @ExceptionHandler(IdDuplicationException.class)
     public ResponseEntity<ErrorResponse> handleIdDuplicationException(IdDuplicationException e){
-        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.BAD_REQUEST);
 
     }
     @ExceptionHandler(IllegalUserException.class)
     public ResponseEntity<ErrorResponse> handleIllegalUserException(IllegalUserException e){
-        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.BAD_REQUEST);
 
     }
     @ExceptionHandler(IntroductionLimitException.class)
     public ResponseEntity<ErrorResponse> handleIntroductionLimitException(IntroductionLimitException e){
-        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.BAD_REQUEST);
 
     }
     @ExceptionHandler(NickDuplicationException.class)
     public ResponseEntity<ErrorResponse> handleNickDuplicationException(NickDuplicationException e){
-        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.BAD_REQUEST);
 
     }
     @ExceptionHandler(NoticeNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNoticeNotFoundException(NoticeNotFoundException e){
-        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.BAD_REQUEST);
 
     }
     @ExceptionHandler(ParagraphCountException.class)
     public ResponseEntity<ErrorResponse> handleParagraphCountException(ParagraphCountException e){
-        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.BAD_REQUEST);
 
     }
     @ExceptionHandler(ParagraphLimitException.class)
     public ResponseEntity<ErrorResponse> handleParagraphLimitException(ParagraphLimitException e){
-        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.BAD_REQUEST);
 
     }
     @ExceptionHandler(ParagraphNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleParagraphNotFoundException(ParagraphNotFoundException e){
-        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.BAD_REQUEST);
 
     }
     @ExceptionHandler(PasswordCheckException.class)
     public ResponseEntity<ErrorResponse> handlePasswordCheckException(PasswordCheckException e){
-        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.BAD_REQUEST);
 
     }
     @ExceptionHandler(PostNotFoundException.class)
     public ResponseEntity<ErrorResponse> handlePostNotFoundException(PostNotFoundException e){
-        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.BAD_REQUEST);
 
     }
 }
