3214dedcdf3a2cd2388906d026aca54c91057025
fnzl08 <nero923@gmail.com>
2022-03-31 18:50:53 +0900

    hj global ex

--- src/main/java/com/example/hanghaefinal/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/hanghaefinal/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.example.hanghaefinal.exception;
 
+import com.example.hanghaefinal.exception.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -45,5 +47,59 @@ public class GlobalExceptionHandler {
                 HttpStatus.BAD_REQUEST
         );
     }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUsernameNotFoundException(UsernameNotFoundException e) {
+        return new ResponseEntity<>(new ErrorResponse("U001", e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AdminOnlyException.class)
+    public ResponseEntity<ErrorResponse> handleAdminOnlyException(AdminOnlyException e){
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+
+    }
+
+    @ExceptionHandler(CommentLimitException.class)
+    public ResponseEntity<ErrorResponse> handleCommentLimitException(CommentLimitException e){
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+
+    }
+    @ExceptionHandler(CommentNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleCommentNotFoundException(CommentNotFoundException e){
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+
+    }
+    @ExceptionHandler(ContentNullException.class)
+    public ResponseEntity<ErrorResponse> handleContentNullException(ContentNullException e){
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+
+    }
+    @ExceptionHandler(EqualPasswordException.class)
+    public ResponseEntity<ErrorResponse> handleEqualPasswordException(EqualPasswordException e){
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+
+    }
+    @ExceptionHandler(AdminOnlyException.class)
+    public ResponseEntity<ErrorResponse> handleAdminOnlyException(AdminOnlyException e){
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+
+    }
+    @ExceptionHandler(AdminOnlyException.class)
+    public ResponseEntity<ErrorResponse> handleAdminOnlyException(AdminOnlyException e){
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+
+    }
+    @ExceptionHandler(AdminOnlyException.class)
+    public ResponseEntity<ErrorResponse> handleAdminOnlyException(AdminOnlyException e){
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+
+    }    @ExceptionHandler(AdminOnlyException.class)
+    public ResponseEntity<ErrorResponse> handleAdminOnlyException(AdminOnlyException e){
+        return new ResponseEntity<>(new ErrorResponse("", e.getMessage()), HttpStatus.UNAUTHORIZED);
+
+    }
+
+
+
 }
 
