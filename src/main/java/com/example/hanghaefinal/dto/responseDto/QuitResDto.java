4d9a4d090e07c819652bbc036bf6e0f8deb7efb8
binscot <binscot0317@gmail.com>
2022-04-04 16:05:39 +0900

    quit 정보전달 변경

--- src/main/java/com/example/hanghaefinal/dto/responseDto/QuitResDto.java
+++ src/main/java/com/example/hanghaefinal/dto/responseDto/QuitResDto.java
@@ -1,13 +1,14 @@
 package com.example.hanghaefinal.dto.responseDto;
 
 import com.example.hanghaefinal.model.Paragraph;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuitResDto {
     private Paragraph.MessageType type;
     private String nickName; // message
